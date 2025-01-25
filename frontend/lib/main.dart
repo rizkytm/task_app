a4785855828dd1ef87abb2055d3ca9ae316ea61d
Rizky TM <ritrime97@gmail.com>
2025-01-25 21:26:38 +0700

    feat: init fe

---
+++ frontend/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/features/auth/pages/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Task App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const SignupPage());
+  }
+}
