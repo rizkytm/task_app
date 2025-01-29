ac910b5adc0b135c534b68616fef68cbeeedfef2
Rizky TM <ritrime97@gmail.com>
2025-01-29 17:06:22 +0700

    feat: finishing auth route

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,44 @@
+import { UUID } from "crypto";
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { db } from "../db";
+import { users } from "../db/schema";
+import { eq } from "drizzle-orm";
+
+export interface AuthRequest extends Request {
+    user?: UUID;
+    token?: string;
+}
+
+export const auth = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) {
+            res.status(401).json({ msg: "Access denied" });
+            return;
+        }
+
+        const verified = jwt.verify(token, "passwordKey");
+
+        if (!verified) {
+            res.status(401).json({ msg: "Token verification failed" });
+            return;
+        }
+
+        const verifiedToken = verified as { id: UUID };
+
+        const [user] = await db.select().from(users).where(eq(users.id, verifiedToken.id));
+
+        if (!user) {
+            res.status(401).json({ msg: "User not found" });
+            return;
+        }
+
+        req.user = verifiedToken.id;
+        req.token = token;
+
+        next();
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+};
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,8 @@ import { db } from "../db";
 import { NewUser, users } from "../db/schema";
 import { eq } from "drizzle-orm";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { auth, AuthRequest } from "../middleware/auth";
 
 const authRouter = Router();
 
@@ -12,13 +14,18 @@ interface SignUpBody {
     password: string;
 }
 
+interface LoginBody {
+    email: string;
+    password: string;
+}
+
 authRouter.post("/signup", async (req: Request<{}, {}, SignUpBody>, res: Response) => {
     try {
         const { name, email, password } = req.body;
 
-        const existingUser = await db.select().from(users).where(eq(users.email, email));
+        const [existingUser] = await db.select().from(users).where(eq(users.email, email));
 
-        if (existingUser.length > 0) {
+        if (existingUser) {
             res.status(400).json({ error: "User already exists" });
             return;
         }
@@ -37,10 +44,78 @@ authRouter.post("/signup", async (req: Request<{}, {}, SignUpBody>, res: Respons
     } catch (e) {
         res.status(500).json({ error: e });
     }
-})
+});
 
-authRouter.get("/", (req, res) => {
-    res.send("Auth");
+authRouter.post("/login", async (req: Request<{}, {}, LoginBody>, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const [existingUser] = await db.select().from(users).where(eq(users.email, email));
+
+        if (!existingUser) {
+            res.status(400).json({ error: "User not exists" });
+            return;
+        }
+
+        const isMatch = await bcryptjs.compare(password, existingUser.password);
+
+        if (!isMatch) {
+            res.status(400).json({ msg: "Invalid credentials" });
+            return;
+        }
+
+        const token = jwt.sign({ id: existingUser.id}, "passwordKey");
+
+        res.json({token, ...existingUser});
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+authRouter.post("/tokenIsValid", async (req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+
+        if (!token) {
+            res.json(false);
+            return;
+        }
+
+        const verified = jwt.verify(token, "passwordKey");
+
+        if (!verified) {
+            res.json(false);
+            return;
+        }
+
+        const verifiedToken = verified as { id: string };
+
+        const [user] = await db.select().from(users).where(eq(users.id, verifiedToken.id));
+
+        if (!user) {
+            res.json(false);
+            return;
+        }
+
+        res.json(true);
+    } catch (e) {
+        res.status(500).json(false);
+    }
+});
+
+authRouter.get("/", auth, async (req: AuthRequest, res) => {
+    try {
+        if (!req.user) {
+            res.status(401).json({ msg: "User not found!" });
+            return;
+        }
+
+        const [user] = await db.select().from(users).where(eq(users.id, req.user));
+
+        res.json({ ...user, token: req.token });
+    } catch (e) {
+        res.status(500).json(false);
+    }
 });
 
 export default authRouter;
