c4779517d81583d2af34a93180333899af23b786
Rizky TM <ritrime97@gmail.com>
2025-01-29 11:39:21 +0700

    feat: init backend

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import authRouter from './routes/auth';
+
+const app = express();
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
