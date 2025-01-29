b81839e9c0ccf3100422e506a616a53049095b9e
Rizky TM <ritrime97@gmail.com>
2025-01-29 15:16:57 +0700

    feat: finishing signup route

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-    connectionString: "postgresql://postgres:test123@mydb:5432/mydb"
+    connectionString: "postgresql://postgres:test123@db:5432/mydb"
 });
 
 export const db = drizzle(pool);
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,43 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { db } from "../db";
+import { NewUser, users } from "../db/schema";
+import { eq } from "drizzle-orm";
+import bcryptjs from "bcryptjs";
 
 const authRouter = Router();
+
+interface SignUpBody {
+    name: string;
+    email: string;
+    password: string;
+}
+
+authRouter.post("/signup", async (req: Request<{}, {}, SignUpBody>, res: Response) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await db.select().from(users).where(eq(users.email, email));
+
+        if (existingUser.length > 0) {
+            res.status(400).json({ error: "User already exists" });
+            return;
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 8);
+
+        const newUser: NewUser = {
+            name,
+            email,
+            password: hashedPassword,
+        };
+
+        const [user] = await db.insert(users).values(newUser).returning();
+
+        res.status(201).json(user);
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+})
 
 authRouter.get("/", (req, res) => {
     res.send("Auth");
